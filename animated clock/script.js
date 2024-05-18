90b5f4c8ab06c5f25e09a3fc2bbe2dbb74ad2f8f
Evgeniia Zhavoronkova <maongaku@gmail.com>
2024-05-18 09:16:25 +0200

    feat: add initial function settings

---
+++ animated clock/script.js
@@ -0,0 +1,10 @@
+function clock() {
+  const cuurentTime = new Date();
+  const canvas = document.getElementById('canvas');
+  const ctx = canvas.getContext('2d');
+
+  // Setup canvas
+  ctx.save();
+  
+  ctx.restore();
+}
