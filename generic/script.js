e2868f4561d83633715fd099739cbf3e596bf40c
Evgeniia Zhavoronkova <maongaku@gmail.com>
2024-07-30 11:53:37 +0200

    chore: remove extra old projects files

--- generic/script.js
+++
@@ -1,9 +0,0 @@
-let arr = [8, 8, 62, 62, 48, 48, 34, 34, 82, 82, 55, 55, 20, 20, 84, 84];
-
-// let newArr = arr.splice(0, 1)
-let newNew = null;
-for (let i = 0; i < arr.length; i++) {
-  console.log(+arr.splice(i, 1)) 
-}
-
-console.log(newNew)
